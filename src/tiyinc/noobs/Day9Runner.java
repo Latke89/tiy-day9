01b8ca2499453a06ed143e9cb9de25fce6758292
Brett Gordon <Brett@Bretts-MacBook-Air.local>
2016-08-18 11:47:44 -0400

    Were writing to file

--- src/tiyinc/noobs/Day9Runner.java
+++ src/tiyinc/noobs/Day9Runner.java
@@ -1,5 +1,9 @@
 package tiyinc.noobs;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
 /**
  * Created by Brett on 8/18/16.
  */
@@ -10,9 +14,48 @@ public class Day9Runner {
 		Day9Runner myRunner = new Day9Runner();
 
 		myRunner.testWriteFile();
+		myRunner.testReadFile();
+	}
+
+	public void testReadFile() {
+		try {
+
+			File testFile = new File("src/tiyinc/noobs/Day9Runner.java");
+			Scanner fileScanner = new Scanner(testFile);
+			int numLines = 0;
+			int numChars = 0;
+			while (fileScanner.hasNext()) {
+				String currentLine = fileScanner.nextLine();
+				numLines++;
+				numChars += currentLine.length();
+				System.out.println(currentLine);
+			}
+			System.out.println("You have " + numLines + " lines in your code");
+			System.out.println("You have " + numChars + " chars in your code");
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
 	}
 
 	public void testWriteFile() {
 		System.out.println("testWriteFile()");
+		FileWriter testWriter = null;
+
+		try{
+			File testFile = new File("noobs.txt");
+			testWriter = new FileWriter(testFile);
+			testWriter.write("Java Rocks! Harder!!!!");
+//			testWriter.close();
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		} finally {
+			if(testWriter != null) {
+				try {
+					testWriter.close();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
 	}
 }
